e06ae55b2bac7c23030075f309d0e1854acd9e4b
pmpyy <pmpyyyoyo2@gmail.com>
2023-10-06 23:15:01 +0700

    gg

--- src/router/index.js
+++ src/router/index.js
@@ -25,11 +25,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
       path: '/discription',
       name: 'discription',
       component: Discription
@@ -38,14 +33,6 @@ const router = createRouter({
       path: '/navmilk',
       name: 'navmilk',
       component: Navmilk
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
